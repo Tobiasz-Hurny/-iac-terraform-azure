eeee6b14cc3dfd0e3ee38a85bead609eede29428
Tobi <75902541+Tobiasz-Hurny@users.noreply.github.com>
2022-05-27 12:39:21 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -12,14 +12,14 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_storage_container" "example" {
-  name                  = var.name_azurerm_storage_container
+  name                  = var.azurerm_storage_container
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = var.access_type_azurerm_storage_container
 }
 
 resource "azurerm_storage_blob" "example" {
   url                    = var.url_azurerm_storage_blob
-  name                   = var.name_azurerm_storage_blob
+  name                   = var.azurerm_storage_blob
   storage_account_name   = azurerm_storage_account.example.name
   storage_container_name = azurerm_storage_container.example.name
   type                   = var.type_azurerm_storage_blob
